ebf4ea808a1b599bd6b6f2d19ec49f478e32bec4
Phil-Grim <140601461+Phil-Grim@users.noreply.github.com>
2023-11-22 15:00:12 +0000

    Update streamlit_app.py
    Adding multiselect to the app

--- streamlit_app.py
+++ streamlit_app.py
@@ -12,4 +12,14 @@ streamlit.text('🥑🍞 Avocado Toast')
 streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
 
 my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt") # using a csv hosted by Snowflake in their AWS S£ bucket
+my_fruit_list = my_fruit_list.set_index('Fruit') # setting Fruit to the dataframe index so that the multiselect function below picks out the Fruit to be displayed as options in the app
+
+# Let's put a pick list here so they can pick the fruit they want to include 
+streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index))
+
+# Display the table on the page.
 streamlit.dataframe(my_fruit_list)
+
+
+
+
